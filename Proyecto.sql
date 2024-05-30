51b778721d04f18c33e617cf5626b7bb17534f5a
HikkizZ <fmiranda.reb@gmail.com>
2024-05-29 21:22:01 -0400

    Se agregaron el resto de tablas del MR. Hay confuciones anotadas a modo de comentario. Se agrega la tabla Eventos, aun no ejecutada para ser creada por dudas con respecto a tipo de dato.
    Desde las relaciones hacia abajo no hay nada ejecutado

--- Proyecto.sql
+++ Proyecto.sql
@@ -38,6 +38,18 @@ CREATE TABLE Patologia(
     pat_descripcion VARCHAR2(30)
 );
 
+CREATE TABLE Evento( -- Sin crear
+    eve_codigo INTEGER PRIMARY KEY,
+    eve_descripcion VARCHAR2(20),
+    eve_hora_inicio VARCHAR2(5), -- Formato HH:MM
+    eve_hora_fin VARCHAR2(5), -- Formato HH:MM
+    eve_valor_arriendo INTEGER,
+    eve_modalidad_pago VARCHAR2(10), -- Preguntar
+    eve_fecha_pago DATE,
+    cli_rut VARCHAR2(12),
+    FOREIGN KEY (cli_rut) REFERENCES Cliente(cli_rut)
+);
+
 CREATE TABLE Cursos(
     cur_codigo INTEGER PRIMARY KEY,
     cur_descripcion VARCHAR2(20),
@@ -88,3 +100,132 @@ CREATE TABLE Sesion(
     FOREIGN KEY (ins_rut) REFERENCES Instructor(ins_rut)
 );
 
+CREATE TABLE Plan( -- Duda con modalidad / Sin crear
+    pla_codigo INTEGER PRIMARY KEY,
+    pla_modalidad INTEGER,
+    pla_valor INTEGER,
+    pro_codigo INTEGER,
+    FOREIGN KEY (pro_codigo) REFERENCES Programa(pro_codigo)
+);
+
+-- Creacion de Relaciones - Sin ejecutar
+CREATE TABLE Padece(
+    pat_codigo INTEGER,
+    cli_rut VARCHAR2(12),
+    PRIMARY KEY (pat_codigo, cli_rut),
+    FOREIGN KEY (pat_codigo) REFERENCES Patologia(pat_codigo),
+    FOREIGN KEY (cli_rut) REFERENCES Cliente(cli_rut)
+);
+
+CREATE TABLE Trabaja(
+    emp_codigo INTEGER,
+    cli_rut VARCHAR2(12),
+    PRIMARY KEY (emp_codigo, cli_rut),
+    FOREIGN KEY (emp_codigo) REFERENCES Empresa(emp_codigo),
+    FOREIGN KEY (cli_rut) REFERENCES Cliente(cli_rut)
+);
+
+CREATE TABLE Posee(
+    est_codigo INTEGER,
+    fecha_inicio DATE,
+    fecha_termino DATE,
+    cli_rut VARCHAR2(12),
+    PRIMARY KEY (est_codigo, fecha_inicio, cli_rut),
+    FOREIGN KEY (est_codigo) REFERENCES Estado(est_codigo),
+    FOREIGN KEY (cli_rut) REFERENCES Cliente(cli_rut)
+);
+
+CREATE TABLE Contrata(
+    pla_codigo INTEGER,
+    cli_rut VARCHAR2(12),
+    fecha DATE,
+    PRIMARY KEY (pla_codigo, cli_rut),
+    FOREIGN KEY (pla_codigo) REFERENCES Plan(pla_codigo),
+    FOREIGN KEY (cli_rut) REFERENCES Cliente(cli_rut)
+);
+
+CREATE TABLE Inscribe(
+    cur_codigo INTEGER,
+    cli_rut VARCHAR2(20),
+    fecha DATE,
+    PRIMARY KEY (cur_codigo, cli_rut),
+    FOREIGN KEY (cur_codigo) REFERENCES Curso(cur_codigo),
+    FOREIGN KEY (cli_rut) REFERENCES Cliente(cli_rut)
+);
+
+CREATE TABLE Reside(
+    com_codigo INTEGER,
+    ins_rut VARCHAR2(12),
+    PRIMARY KEY (com_codigo, ins_rut),
+    FOREIGN KEY (com_codigo) REFERENCES Comuna(com_codigo),
+    FOREIGN KEY (ins_rut) REFERENCES Instructor(ins_rut)
+);
+
+CREATE TABLE Se_da(
+    cur_codigo INTEGER,
+    ses_codigo INTEGER,
+    PRIMARY KEY (cur_codigo, ses_codigo),
+    FOREIGN KEY (cur_codigo) REFERENCES Curso(cur_codigo),
+    FOREIGN KEY (ses_codigo) REFERENCES Sesion(ses_codigo)
+);
+
+CREATE TABLE Se_da_dos(
+    pro_codigo INTEGER,
+    ses_codigo INTEGER,
+    PRIMARY KEY (pro_codigo, ses_codigo),
+    FOREIGN KEY (pro_codigo) REFERENCES Programa(pro_codigo),
+    FOREIGN KEY (ses_codigo) REFERENCES Sesion(ses_codigo)
+);
+
+CREATE TABLE Agenda(
+    ses_codigo INTEGER,
+    cli_rut VARCHAR2(12),
+    PRIMARY KEY (ses_codigo, cli_rut),
+    FOREIGN KEY (ses_codigo) REFERENCES Sesion(ses_codigo),
+    FOREIGN KEY (cli_rut) REFERENCES Cliente(cli_rut)
+);
+
+CREATE TABLE Arrienda(
+    sal_codigo INTEGER,
+    eve_codigo INTEGER,
+    PRIMARY KEY (sal_codigo, eve_codigo),
+    FOREIGN KEY (sal_codigo) REFERENCES Salon(sal_codigo),
+    FOREIGN KEY (eve_codigo) REFERENCES Evento(eve_codigo)
+);
+
+CREATE TABLE Imparte(
+    ins_codigo INTEGER,
+    cur_codigo INTEGER,
+    PRIMARY KEY (ins_codigo, cur_codigo),
+    FOREIGN KEY (ins_codigo) REFERENCES Instructor(ins_codigo),
+    FOREIGN KEY (cur_codigo) REFERENCES Curso(cur_codigo)
+);
+
+CREATE TABLE Es_asociado(
+    pro_codigo INTEGER,
+    ins_rut VARCHAR2(12),
+    PRIMARY KEY (pro_codigo, ins_rut),
+    FOREIGN KEY (pro_codigo) REFERENCES Programa(pro_codigo),
+    FOREIGN KEY (ins_rut) REFERENCES Instructor(ins_rut)
+);
+
+CREATE TABLE Inscribe_dos(
+    pro_codigo INTEGER,
+    fecha DATE,
+    cli_rut VARCHAR2(12),
+    PRIMARY KEY (pro_codigo, cli_rut),
+    FOREIGN KEY (pro_codigo) REFERENCES Programa(pro_codigo),
+    FOREIGN KEY (cli_rut) REFERENCES Cliente(cli_rut)
+);
+
+-- SubCategorias (?) - Sin ejecutar
+CREATE TABLE Terapia(
+    pro_codigo INTEGER,
+    PRIMARY KEY (pro_codigo),
+    FOREIGN KEY (pro_codigo) REFERENCES Programa(pro_codigo)
+);
+
+CREATE TABLE Especialidad(
+    esp_codigo INTEGER PRIMARY KEY,
+    esp_nombre VARCHAR2(20)
+);
